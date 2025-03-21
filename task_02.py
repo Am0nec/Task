f84a2741c4f723529bf95ef3023b32dcddeef658
Am0nec <113848987+Am0nec@users.noreply.github.com>
2025-03-21 16:15:05 +0300

    Update task_02.py

--- task_02.py
+++ task_02.py
@@ -1,22 +1,24 @@
-def coincidence(subsequence, range_list):
+def coincidence(subsequence = None, range_list = None):
     final_list = []
 
-    if subsequence == None or range_list == None:
+    if subsequence is None or range_list is None:
         return final_list
 
     if len(subsequence) == 0 or len(range_list) < 2:
         return final_list
 
+    range_start = range_list[0]
+    range_end = range_list[-1] - 1
+
     for i in subsequence:
         if not isinstance(i, (int, float)):
             continue
-        elif (range_list[0] <= i) and (i <= range_list[1]):
-            if i == range_list[1]:
-                continue
+        elif range_start <= i <= range_end:
             final_list.append(i)
     return final_list
 
-started_list = [1, 4]
-range_list = None
+started_list = [None, 1, 'foo', 4, 2, 2.5]
+range_list = (1, 4)
 x = coincidence(started_list, range_list)
 print(x)
+
